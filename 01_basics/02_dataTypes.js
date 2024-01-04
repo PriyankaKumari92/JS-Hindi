cdc6d49bc9adc65c723269294cf0bf38df62d60a
Priyanka Kumari <63950768+PriyankaKumari92@users.noreply.github.com>
2024-01-04 13:35:27 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" // Treat as JS code in newer version
+
+//  alert(3+3)  // we are using nodeJs not browser
+
+console.log(3+
+    3)  // code readiblity should be high
+
+    console.log("Priyanka") 
+
+
+
+let name = " Priyanka"
+let age = 30
+let state = null
+let isLoggedIn = false
+
+//permitive data type 7
+// Number => 2 to the power 53
+//bigint => larger number for stock market
+//string => " "
+//boolean => true/false
+//null => standalone value
+// undifined => undifned
+//symbol => unique 
+
+
+//non permitive datatype
+
+//object
+//function
+
+console.log(typeof name)
+console.log(typeof null)
+
+// type of when we need to check for null = object, undefined = undeifined 
