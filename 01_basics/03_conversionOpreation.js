575e533bf59a08d5bf296da7397718f70046d7aa
Priyanka Kumari <63950768+PriyankaKumari92@users.noreply.github.com>
2024-01-05 09:16:30 +0000

    conversion

---
+++ 01_basics/03_conversionOpreation.js
@@ -0,0 +1,31 @@
+let score = "Priyanka"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33 => number, 33
+// "33abc" => NaN
+// True => 1; False => 0
+
+let isLoggedIn = "Priyanka"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// " " => false
+// "priyanka" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
