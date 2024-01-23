84cb638760581c7a55d95b9266f87fd39746fa42
Priyanka Kumari <63950768+PriyankaKumari92@users.noreply.github.com>
2024-01-23 15:02:18 +0000

    jsn-object

---
+++ 02_basics/05_object_api.js
@@ -0,0 +1,41 @@
+// object destructured method
+
+const course = {
+    coursename: "js in hindhi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+console.log(course.courseInstructor);
+
+
+// DESTRUCTURED both output is same "hitesh"
+
+const {courseInstructor : insturctor} = course
+
+console.log(insturctor);
+
+
+// api get from object  key value always strining : it call by JSN
+
+// {
+//  "name": "priyanka",
+//  "course": "js in hindhi",
+//  "price": "free"
+
+// }
+
+// api also get by array with multiple api
+
+[
+
+{
+
+},
+{},
+{}
+]
+
+// https://jsonformatter.org/
+// https://randomuser.me/
+//https://randomuser.me/api/
