527226665b9e09fa2e69bb1dad297b8bb509a629
Priyanka Kumari <63950768+PriyankaKumari92@users.noreply.github.com>
2024-01-31 15:47:46 +0000

    while and dowhile

---
+++ 05_iterations/while.js
@@ -0,0 +1,23 @@
+let index = 0;
+
+while (index <= 10) {
+    console.log(`value is ${index}`)
+    index = index + 2
+}
+
+
+const myArray = ["Flash", " superman", " spiderman"]
+
+let array = 0 
+while (array < myArray.length) {
+ 
+    console.log(`value is ${myArray[array]}`)
+     array = array + 1
+}
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
